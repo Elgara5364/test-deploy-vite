6c5da44d8e4d78f5e8af5a84f22827ddfb386aab
Elgara5364 <steveloardi5364@gmail.com>
2024-01-30 23:24:06 +0900

    rm ecosystem.config.js

--- ecosystem.config.js
+++
@@ -1,9 +0,0 @@
- {
-  apps : [
-        {
-                name   : "binar_car_rental",
-                script: "npm",
-                args: "serve -s build -p 3000",
-                env:{
-                        NODE_ENV: "production"
-        }
