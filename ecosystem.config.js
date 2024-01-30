17c4873dd58eb756385106b105c2e8afc2216ab2
Elgara5364 <steveloardi5364@gmail.com>
2024-01-31 00:12:28 +0900

    change port on ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "react-gold", // ubah sesuai nama aplikasi yang dibuat
-      script: "serve",
+      script: "npx",
       // sesuaikan port yang telah diubah
       args: "serve -s build -p 3001 build",
       interpreter: "none",
