0747e1e8b213fb7eed319def2227580cbcbc59a2
Steve Loardi <51954323+Elgara5364@users.noreply.github.com>
2024-01-30 22:03:03 +0800

    add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps : [
+        {
+                name   : "binar_car_rental",
+                script: "npm",
+                args: "serve -s build -p 3000",
+                env:{
+                        NODE_ENV: "production"
+        }
