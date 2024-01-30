937048d9a3aef4bd1abe35c1a23c748696f09f91
Elgara5364 <steveloardi5364@gmail.com>
2024-01-30 23:51:04 +0900

    add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "react-gold", // ubah sesuai nama aplikasi yang dibuat
+      script: "npx",
+      // sesuaikan port yang telah diubah
+      args: "serve -s -l 3000 build",
+      interpreter: "none",
+      watch: true,
+      merge_logs: true,
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
