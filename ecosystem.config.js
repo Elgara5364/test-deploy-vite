52457bc7b6b9238a5f059fc917226c15099f72b8
Elgara5364 <steveloardi5364@gmail.com>
2024-01-31 14:15:03 +0900

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,6 @@ module.exports = {
     {
       name: "react-gold",
       script: "npm start",
-      args: " npm start",
       error_file: "./pm2-error.log",
       out_file: "./pm2-out.log",
     },
