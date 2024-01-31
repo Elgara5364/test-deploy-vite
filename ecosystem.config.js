5776b2749f9aa2792bd47a9dfac6521291538151
Elgara5364 <steveloardi5364@gmail.com>
2024-01-31 13:58:59 +0900

    test

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "react-gold",
+      script: "npm start",
+      error_file: "./pm2-error.log",
+      out_file: "./pm2-out.log",
+    },
+  ],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
-    }
+      "@": resolve(__dirname, "./src"),
+    },
   },
-})
+});
