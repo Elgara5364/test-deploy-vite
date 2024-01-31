0e9936e22328c8e43cda5357a7d48f3822c3b9c5
Elgara5364 <steveloardi5364@gmail.com>
2024-01-31 13:33:30 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "jquery";
-import "popper.js";
-import "bootstrap/dist/js/bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "jquery";
+// import "popper.js";
+// import "bootstrap/dist/js/bootstrap";
 import { routes as LandingRoutes } from "@/modules/Landing/routes";
 import { useRoutes } from "react-router-dom";
 
