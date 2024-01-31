e4171ce4c65b213eff8c7bc10af3c8e5629b16bc
Elgara5364 <steveloardi5364@gmail.com>
2024-01-31 14:01:27 +0900

    test

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  build: { chunkSizeWarningLimit: 1600 },
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
